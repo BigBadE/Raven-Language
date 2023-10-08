9a6c81a0ceddab663affe502d092264bff6e8056
BigBadE <bigbade333@gmail.com>
2023-10-08 14:42:33 -0500

    Debugging vtable calling.

--- language/compilers/llvm/src/internal/instructions.rs
+++ language/compilers/llvm/src/internal/instructions.rs
@@ -1,7 +1,7 @@
 use inkwell::builder::Builder;
 use inkwell::{AddressSpace, IntPredicate};
 use inkwell::types::{BasicType, BasicTypeEnum};
-use inkwell::values::{BasicMetadataValueEnum, BasicValue, BasicValueEnum, FunctionValue, PointerValue};
+use inkwell::values::{BasicMetadataValueEnum, BasicValue, BasicValueEnum, CallableValue, FunctionValue, PointerValue};
 use crate::compiler::CompilerImpl;
 use crate::internal::intrinsics::compile_llvm_intrinsics;
 use crate::type_getter::CompilerTypeGetter;
@@ -84,12 +84,21 @@ pub fn compile_internal<'ctx>(type_getter: &CompilerTypeGetter<'ctx>, compiler: 
         };
 
         let malloc = compiler.builder.build_call(compiler.module.get_function("malloc")
-                                                                 .unwrap_or(compile_llvm_intrinsics("malloc", type_getter)),
-                                                             &[BasicMetadataValueEnum::PointerValue(size)], "1")
+                                                     .unwrap_or(compile_llvm_intrinsics("malloc", type_getter)),
+                                                 &[BasicMetadataValueEnum::PointerValue(size)], "1")
             .try_as_basic_value().unwrap_left().into_pointer_value();
 
         compiler.builder.build_store(malloc, compiler.context.i64_type().const_zero());
         compiler.builder.build_return(Some(&malloc.as_basic_value_enum()));
+    } else if name.starts_with("main::test_internal") {
+        let calling = compiler.module.get_global("str_vtable").unwrap().as_pointer_value();
+        let calling = compiler.builder.build_bitcast(calling,
+                                                     compile_llvm_intrinsics("printf", type_getter).get_type().ptr_type(AddressSpace::default()).ptr_type(AddressSpace::default()), "2");
+        println!("Found {}", calling.get_type());
+        let calling = compiler.builder.build_load(calling.into_pointer_value(), "4");
+        compiler.builder.build_call(CallableValue::try_from(calling.into_pointer_value()).unwrap(),
+        &[From::from(compiler.context.const_string("Testing".as_bytes(), true))], "1");
+        compiler.builder.build_return(None);
     } else {
         panic!("Unknown internal operation: {}", name)
     }
