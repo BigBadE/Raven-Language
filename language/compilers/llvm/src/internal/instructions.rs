4b5ead9a6bcce4c98969b0d7bb117cf2369ffdc6
Jeremy924 <Jeremyleicht@outlook.com>
2023-10-30 10:02:21 -0500

    Working on bitwise operators

--- language/compilers/llvm/src/internal/instructions.rs
+++ language/compilers/llvm/src/internal/instructions.rs
@@ -206,11 +206,10 @@ pub fn compile_internal<'ctx>(type_getter: &CompilerTypeGetter<'ctx>, compiler: 
         let malloc = malloc_type(type_getter, pointer_type.get_type().const_zero(), &mut 0);
 
         let returning = compiler.builder.build_xor(compiler.builder.build_load(pointer_type, "2").into_int_value(),
-                                                       compiler.builder.build_load(params.get(1).unwrap().into_pointer_value(), "3").into_int_value(), "1");
+                                                   compiler.builder.build_load(params.get(1).unwrap().into_pointer_value(), "3").into_int_value(), "1");
         compiler.builder.build_store(malloc, returning);
         compiler.builder.build_return(Some(&malloc));
-    }
-    else {
+    } else {
         panic!("Unknown internal operation: {}", name)
     }
 }
