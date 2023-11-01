7ba0fddf79c390db49495d19f158d570ac297545
BigBadE <bigbade333@gmail.com>
2023-11-01 16:42:46 -0500

    More debugging

--- language/parser/src/parser/util.rs
+++ language/parser/src/parser/util.rs
@@ -83,6 +83,7 @@ impl<'a> ParserUtils<'a> {
 
     async fn add_implementation(syntax: Arc<Mutex<Syntax>>, implementor: TraitImplementor,
                                 resolver: Box<dyn NameResolver>, process_manager: Box<dyn ProcessManager>) -> Result<(), ParsingError> {
+        println!("Running!");
         let mut generics = IndexMap::new();
         for (generic, bounds) in implementor.generics {
             let mut final_bounds = Vec::new();
