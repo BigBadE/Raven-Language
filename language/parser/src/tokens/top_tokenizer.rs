2797a5aa8be43fe393619e3098453a470d9b0c7e
a11155 <terminatorpro11155@gmail.com>
2023-11-03 17:19:48 -0500

    Fixed \n at the end of file

--- language/parser/src/tokens/top_tokenizer.rs
+++ language/parser/src/tokens/top_tokenizer.rs
@@ -69,6 +69,12 @@ pub fn next_top_token(tokenizer: &mut Tokenizer) -> Token {
         } else if tokenizer.state == TokenizerState::TOP_ELEMENT_TO_STRUCT {
             // Looking for a field name inside a struct
             parse_to_character(tokenizer, TokenTypes::FieldName, &[b':', b'='])
+        } else if tokenizer.state == TokenizerState::TOP_ELEMENT && tokenizer.matches("")  {
+            // If there are blank lines after all of the elements, skip them
+          
+            println!("hi");
+            tokenizer.index += 1;
+            tokenizer.make_token(TokenTypes::BlankLine)
         } else {
             tokenizer.handle_invalid()
         },
