3f4160ee62bc74b437274d4b0db9d974504d83bf
Big_Bad_E <bigbade333@gmail.com>
2023-11-08 16:57:42 -0600

    More semantic token work

--- tools/ide-plugins/raven-language-server/src/main.rs
+++ tools/ide-plugins/raven-language-server/src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
 use std::error::Error;
 
-use lsp_server::{Connection, ExtractError, Message, Request, RequestId};
-use lsp_types::{InitializeParams, SemanticTokensFullOptions, SemanticTokensLegend, SemanticTokensOptions, SemanticTokensServerCapabilities, SemanticTokenType, ServerCapabilities};
+use lsp_server::{Connection, ExtractError, Message, Notification, Request, RequestId};
+use lsp_types::{InitializeParams, SemanticTokensFullOptions, SemanticTokensLegend, SemanticTokensOptions, SemanticTokensServerCapabilities, SemanticTokenType, ServerCapabilities, TextDocumentSyncCapability, TextDocumentSyncKind, Url};
+use lsp_types::notification::{DidChangeTextDocument, DidOpenTextDocument};
 use lsp_types::request::SemanticTokensFullRequest;
 use tokio::runtime::Builder;
 
@@ -30,6 +32,7 @@ pub fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
             range: None,
             full: Some(SemanticTokensFullOptions::Bool(true)),
         })),
+        text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
         ..Default::default()
     }).unwrap();
     let initialization_params = connection.initialize(server_capabilities)?;
@@ -43,13 +46,13 @@ fn main_loop(
     params: serde_json::Value,
 ) -> Result<(), Box<dyn Error + Sync + Send>> {
     let pool = Builder::new_multi_thread().build().unwrap();
+    let mut documents: HashMap<Url, String> = HashMap::new();
     let params: InitializeParams = serde_json::from_value(params).unwrap();
     if params.capabilities.text_document.clone().unwrap().semantic_tokens.unwrap().augments_syntax_tokens.unwrap() {
         panic!("Augmenting syntax tokens!")
     }
 
     for msg in &connection.receiver {
-        eprintln!("got msg: {msg:?}");
         match msg {
             Message::Request(req) => {
                 if connection.handle_shutdown(&req)? {
@@ -58,7 +61,8 @@ fn main_loop(
                 eprintln!("got request: {req:?}");
                 match cast::<SemanticTokensFullRequest>(req) {
                     Ok((id, params)) => {
-                        pool.spawn(parse_semantic_tokens(id, params.text_document.uri, connection.sender.clone()));
+                        pool.spawn(parse_semantic_tokens(id, documents[&params.text_document.uri].clone(),
+                                                         connection.sender.clone()));
                         continue;
                     }
                     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
@@ -70,7 +74,24 @@ fn main_loop(
                 eprintln!("got response: {resp:?}");
             }
             Message::Notification(not) => {
-                eprintln!("got notification: {not:?}");
+                eprintln!("got notif: {not:?}");
+                let not = match cast_not::<DidOpenTextDocument>(not) {
+                    Ok((_id, params)) => {
+                        documents.insert(params.text_document.uri, params.text_document.text);
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
+                let _not = match cast_not::<DidChangeTextDocument>(not) {
+                    Ok((_id, params)) => {
+                        // Assume it's only one thing being changed across the whole document
+                        documents.insert(params.text_document.uri, params.content_changes[0].text.clone());
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
             }
         }
     }
@@ -80,6 +101,15 @@ fn main_loop(
 fn cast<R>(req: Request) -> Result<(RequestId, R::Params), ExtractError<Request>>
     where
         R: lsp_types::request::Request,
+        R::Params: serde::de::DeserializeOwned,
+{
+    req.extract(R::METHOD)
+}
+
+
+fn cast_not<R>(req: Notification) -> Result<(RequestId, R::Params), ExtractError<Notification>>
+    where
+        R: lsp_types::notification::Notification,
         R::Params: serde::de::DeserializeOwned,
 {
     req.extract(R::METHOD)
--- tools/ide-plugins/raven-language-server/src/semantic_tokens.rs
+++ tools/ide-plugins/raven-language-server/src/semantic_tokens.rs
@@ -1,17 +1,12 @@
-use std::fs;
 use crossbeam_channel::Sender;
 use lsp_server::{Message, RequestId, Response};
-use lsp_types::{SemanticToken, SemanticTokens, SemanticTokensResult, Url};
+use lsp_types::{SemanticToken, SemanticTokens, SemanticTokensResult};
+
 use parser::tokens::tokenizer::Tokenizer;
 use parser::tokens::tokens::{Token, TokenTypes};
-use urlencoding::decode;
 
-pub async fn parse_semantic_tokens(id: RequestId, file: Url, sender: Sender<Message>) {
-    let mut url = decode(file.path()).unwrap().to_string();
-    url.remove(0);
-
-    let buffer = fs::read(url).unwrap();
-    let mut tokenizer = Tokenizer::new(buffer.as_slice());
+pub async fn parse_semantic_tokens(id: RequestId, file: String, sender: Sender<Message>) {
+    let mut tokenizer = Tokenizer::new(file.as_bytes());
     let mut tokens = Vec::new();
     loop {
         tokens.push(tokenizer.next());
@@ -23,22 +18,22 @@ pub async fn parse_semantic_tokens(id: RequestId, file: Url, sender: Sender<Mess
     let mut last: Option<Token> = None;
     let data = tokens.into_iter().map(|mut token| {
         if token.start.0 != token.end.0 {
+            token.start_offset = token.end_offset - token.end.1 as usize;
             token.start = (token.end.0, 0);
         }
         let delta_line = (token.start.0 - 1) - last.clone().map(|inner| inner.start.0 - 1).unwrap_or(0);
-        eprintln!("Line ({}, {}) to ({}, {}) for {:?} ({})", token.start.0, token.start.1, token.end.0, token.end.1, token.token_type,
-                  token.start_offset - last.clone().map(|inner| inner.end_offset).unwrap_or(0));
         let temp = SemanticToken {
             delta_line,
             delta_start: if delta_line == 0 {
-                (token.start_offset - last.clone().map(|inner| inner.start_offset).unwrap_or(0)) / 2
+                (token.start_offset - last.clone().map(|inner| inner.start_offset).unwrap_or(0))
             } else {
                 0
-            } as u32 * 2,
+            } as u32,
             length: (token.end_offset - token.start_offset) as u32,
             token_type: get_token(&token.token_type),
             token_modifiers_bitset: 0,
         };
+        //eprintln!("Line ({}, {}) to ({}, {}) for {:?} ({:?})", token.start.0, token.start.1, token.end.0, token.end.1, token.token_type,temp);
         last = Some(token);
         temp
     }).collect::<Vec<_>>();
@@ -53,10 +48,10 @@ pub async fn parse_semantic_tokens(id: RequestId, file: Url, sender: Sender<Mess
 
 fn get_token(token_type: &TokenTypes) -> u32 {
     let temp = match token_type {
-        TokenTypes::Identifier => 1,
+        TokenTypes::Identifier | TokenTypes::ReturnType => 1,
         TokenTypes::Variable => 9,
         TokenTypes::ImportStart | TokenTypes::Return | TokenTypes::New | TokenTypes::Modifier |
-        TokenTypes::ReturnType | TokenTypes::FunctionStart => 15,
+        TokenTypes::FunctionStart => 15,
         TokenTypes::Comment => 17,
         TokenTypes::StringStart | TokenTypes::StringEnd | TokenTypes::StringEscape => 18,
         TokenTypes::ReturnTypeArrow | TokenTypes::ImportEnd => 100,
