32e790ac416338c4b01e180a10b7d48f92aacdb5
Pillber <melcard16@gmail.com>
2024-03-08 16:28:00 -0600

    make test folder path platform independent

--- tools/magpie/src/test.rs
+++ tools/magpie/src/test.rs
@@ -4,6 +4,7 @@ mod test {
     use parser::FileSourceSet;
     use data::{Arguments, CompilerArguments, RunnerSettings};
     use std::{env, path, fs};
+    use std::path::PathBuf;
 
     /// Tests directory
     //static TESTS: str = "../lib/test/test:";
@@ -11,11 +12,12 @@ mod test {
     /// Main test
     #[test]
     pub fn test_magpie() {
-        test_recursive("../../lib/test/test");
+        let test_folder: PathBuf = ["..", "..", "lib", "test", "test"].iter().collect();
+        test_recursive(test_folder);
     }
 
     /// Recursively searches for files in the test folder to run as a test
-    fn test_recursive(path: &str) {
+    fn test_recursive(path: PathBuf) {
         for entry in fs::read_dir(path).unwrap() {
 
             let entry = entry.unwrap();
@@ -26,8 +28,8 @@ mod test {
                     println!("File {} doesn't have the right file extension!", mod_path);
                     continue;
                 }
+                let mod_path = format!("{}::test", &mod_path[path.parent().unwrap().to_str().unwrap().len()+6..mod_path.len() - 3]);
                 println!("Running {}", mod_path);
-                let mod_path = format!("{}::test", &mod_path[path.parent().unwrap().to_str().unwrap().len()+2..mod_path.len() - 3]);
                 let mut arguments = Arguments::build_args(
                     false,
                     RunnerSettings {
@@ -52,7 +54,7 @@ mod test {
                     Err(()) => assert!(false, "Failed to compile test {}!", mod_path),
                 }
             } else if path.is_dir() { // supposedly, this is a sub-directory in the test folder
-                test_recursive(path.to_str().unwrap());
+                test_recursive(path);
             } else {
                 println!("Unknown element in test folder!");
                 continue;
