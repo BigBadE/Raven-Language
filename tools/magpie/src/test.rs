665168d89309f048635dd91cea3800fbd333cd8a
Big_Bad_E <bigbade333@gmail.com>
2024-03-10 21:24:46 -0500

    Fix import order

--- tools/magpie/src/test.rs
+++ tools/magpie/src/test.rs
@@ -19,16 +19,17 @@ mod test {
     /// Recursively searches for files in the test folder to run as a test
     fn test_recursive(path: PathBuf) {
         for entry in fs::read_dir(path).unwrap() {
-
             let entry = entry.unwrap();
             let path = entry.path();
-            if path.is_file() { // supposedly, this is a test file
+            if path.is_file() {
+                // supposedly, this is a test file
                 let mod_path = path.to_str().unwrap().replace(path::MAIN_SEPARATOR, "::");
                 if !mod_path.ends_with(".rv") {
                     println!("File {} doesn't have the right file extension!", mod_path);
                     continue;
                 }
-                let mod_path = format!("{}::test", &mod_path[path.parent().unwrap().to_str().unwrap().len()+6..mod_path.len() - 3]);
+                let mod_path =
+                    format!("{}::test", &mod_path[path.parent().unwrap().to_str().unwrap().len() + 6..mod_path.len() - 3]);
                 println!("Running {}", mod_path);
                 let mut arguments = Arguments::build_args(
                     false,
@@ -53,7 +54,8 @@ mod test {
                     },
                     Err(()) => assert!(false, "Failed to compile test {}!", mod_path),
                 }
-            } else if path.is_dir() { // supposedly, this is a sub-directory in the test folder
+            } else if path.is_dir() {
+                // supposedly, this is a sub-directory in the test folder
                 test_recursive(path);
             } else {
                 println!("Unknown element in test folder!");
